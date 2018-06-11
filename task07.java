23284974b8a7fbc41d7bab6d37f752add64fcbb0
vilasha <mariavya@gmail.com>
2018-06-11 02:19:24 +0100

    task07.java:	Java Loops II

---
+++ task07.java
@@ -0,0 +1,48 @@
+/*  Java Loops II
+	We use the integers , , and  to create the following series:
+
+	You are given  queries in the form of , , and . For each query, print the series corresponding to the given , , and  values as a single line of  space-separated integers.
+
+	Input Format
+
+	The first line contains an integer, , denoting the number of queries. 
+	Each line  of the  subsequent lines contains three space-separated integers describing the respective , , and  values for that query.
+
+	Constraints
+
+	Output Format
+
+	For each query, print the corresponding series on a new line. Each series must be printed in order as a single line of  space-separated integers.
+
+	Sample Input
+
+	2
+	0 2 10
+	5 3 5
+	Sample Output
+
+	2 6 14 30 62 126 254 510 1022 2046
+	8 14 26 50 98 */
+
+import java.util.*;
+import java.io.*;
+
+class Solution{
+    public static void main(String []argh){
+        Scanner in = new Scanner(System.in);
+        int t = in.nextInt();
+        for(int i=0; i<t; i++){
+            int a = in.nextInt();
+            int b = in.nextInt();
+            int n = in.nextInt();
+            for (int j = 1; j < n + 1; j++) {
+                int res = a;
+                for (int k = 0; k < j; k++)
+                    res += b * Math.pow(2, k);
+                System.out.print(res + " ");
+            }
+            System.out.println();
+        }
+        in.close();
+    }
+}
