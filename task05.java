48e89938bf34cef18dac87d18cfa8ca7524bc53b
vilasha <mariavya@gmail.com>
2018-06-11 02:23:10 +0100

    task05.java:	Java Output Formatting

--- task05.java
+++ task05.java
@@ -28,7 +28,7 @@
 	================================
 	Explanation
 
-	Each String is left-justified with trailing whitespace through the first  characters. The leading digit of the integer is the  character, and each integer that was less than  digits now has leading zeroes. */
+	Each String is left-justified with trailing whitespace through the first  characters. The leading digit of the integer is the  character, and each integer that was less than  digits now has leading zeroes.  */
 import java.util.Scanner;
 
 public class Solution {
