a3977c61f01d6645034e3c8ca0c5ad068ff9aa28
vilasha <mariavya@gmail.com>
2018-06-12 23:55:43 +0100

    task17.java:	Java String Reverse

---
+++ task17.java
@@ -0,0 +1,29 @@
+/*  Java String Reverse
+	Given a string , print Yes if it is a palindrome, print No otherwise.
+
+	Constraints
+
+	 will consist at most  lower case english letters.
+	Sample Input
+
+	madam
+	Sample Output
+
+	Yes  */
+	
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        
+        Scanner sc=new Scanner(System.in);
+        String A = sc.next();
+        String reverse = new StringBuffer(A).reverse().toString();
+        if (A.equals(reverse))
+            System.out.println("Yes");
+        else
+            System.out.println("No");
+    }
+}
